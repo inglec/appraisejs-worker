7e2a8d6fbc5c4f321ff8bec5109ed1ddaf127164
inglec <inglec@tcd.ie>
2019-02-09 14:09:00 +0000

    Rename GitHub archive after downloading

--- src/utils/github_api.js
+++ src/utils/github_api.js
@@ -1,12 +1,27 @@
 const download = require('download');
+const fs = require('fs');
 const path = require('path');
+const util = require('util');
 
-const downloadArchive = (tokenType, token, owner, repository, commitId, dir) => {
+const rename = util.promisify(fs.rename);
+
+const downloadArchive = (tokenType, token, owner, repository, commitId, rootDir) => {
   const url = `https://api.github.com/repos/${owner}/${repository}/tarball/${commitId}`;
-  return download(url, dir, {
+  const dir = path.join(rootDir, owner, repository);
+  const promise = download(url, dir, {
     extract: true,
     headers: { 'Authorization': `${tokenType} ${token}` },
   });
+
+  return promise
+    .then((data) => {
+      // Rename archive to its commit hash.
+      const archiveName = data[0].path.split(path.sep)[0];
+      const oldPath = path.join(dir, archiveName);
+      const newPath = path.join(dir, commitId);
+
+      return rename(oldPath, newPath);
+    });
 }
 
 module.exports = { downloadArchive };
