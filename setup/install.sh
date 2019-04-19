79d16807f125fa37d21ac9960eb543416de44cf8
Ciarán Ingle <inglec@tcd.ie>
2019-04-19 12:52:01 +0100

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -6,7 +6,7 @@ echo "installing Node.js and NPM"
 apt install -y nodejs npm
 
 echo "installing docker-ce"
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt install -y docker-ce docker-ce-cli containerd.io
